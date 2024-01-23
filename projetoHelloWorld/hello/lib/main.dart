10e41fd8771f454e5c30a1a9e1e979dbb1d9d62a
Isis Pedroni <isispedroni3@gmail.com>
2024-01-22 21:40:00 -0300

    alteração projeto hello

---
+++ projetoHelloWorld/hello/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          'Hello Word',
+          style: TextStyle(fontSize: 16, color: Colors.amber),
+        ),
+      ),
+    ),
+  );
+}
