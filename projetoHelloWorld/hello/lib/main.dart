513da291924bd1a520dd334dd978d4115fd573cf
Isis Pedroni <isispedroni3@gmail.com>
2024-01-24 21:16:04 -0300

    scaffold

--- projetoHelloWorld/hello/lib/main.dart
+++ projetoHelloWorld/hello/lib/main.dart
@@ -4,12 +4,16 @@ void main() {
   // runApp(const MyApp());
   runApp(
     MaterialApp(
-      home: Center(
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body:  Center(
         child: Text(
           'Hello Word',
-          style: TextStyle(fontSize: 16, color: Colors.amber),
+          style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 255, 57, 7)),
         ),
       ),
-    ),
+    )),
   );
 }
