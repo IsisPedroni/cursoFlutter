da772a769bec8107031b81659e43a34a7058c350
Isis Pedroni <isispedroni3@gmail.com>
2024-01-25 20:22:56 -0300

    stateless widget

--- projetoHelloWorld/hello/lib/main.dart
+++ projetoHelloWorld/hello/lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(const MyApp());
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+  // runApp(
+  //   MaterialApp(
+  //       home: Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Hello World'),
+  //     ),
+  //     body: const Center(
+  //       child: Text(
+  //         'Hello Word',
+  //         style:
+  //             TextStyle(fontSize: 16, color: Color.fromARGB(255, 255, 57, 7)),
+  //       ),
+  //     ),
+  //   )),
+  // );
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello World'),
-      ),
-      body:  Center(
-        child: Text(
-          'Hello Word',
-          style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 255, 57, 7)),
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello Word',
+            style:
+                TextStyle(fontSize: 16, color: Color.fromARGB(255, 255, 57, 7)),
+          ),
         ),
       ),
-    )),
-  );
+    );
+  }
 }
